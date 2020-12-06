8deb3d54e14419c2ca575de8b43db883fc9b03c2
brotato1-cmd <martinez.nick.r@gmail.com>
2020-12-06 15:42:37 -0600

    logging again

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import javax.swing.JFrame;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import View.DiagPanel;
 
 public class Main{
+    private static final Logger log = LogManager.getLogger(Main.class);
     public static void main(String [] args){
-
+        log.info("Program starting");
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setTitle("Cancer Diagnosis Program");
